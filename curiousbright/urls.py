4f150b5fc838001290e71eb9849d10ccf996dccd
Ariyo Israel Gbemisola <igbemisola53@gmail.com>
2026-05-18 10:26:17 -0400

    fixed sitemap issue

--- curiousbright/urls.py
+++ curiousbright/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from blog.feeds import LatestPostsFeed
 from blog import admin_views
-from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import views as sitemap_views
 from blog.sitemaps import PostSitemap, CategorySitemap, StaticPageSitemap, StaticViewSitemap
 
 sitemaps = {
@@ -22,7 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('sitemap.xml', sitemap_views.index, {'sitemaps': sitemaps}),
+    path('sitemap-<section>.xml', sitemap_views.sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     path('feed/', LatestPostsFeed(), name='feed'),
 ]
 
